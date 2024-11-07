4d4ab626f9fb71af40e59da6ee7de9c2bcc5b47d
anik60000 <aaanikuu@gmail.com>
2024-11-07 16:23:48 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,6 @@
-var numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
-
-var sum = 0;
-
-for (i=0;i<15;1++)(
-    shuma += numrat[1];
-)
-
-console.log(shuma)
-
-var shuma2=0;
-for(x in numrat){
-    shuma2+=x;
+function validate(){
+    var name = document.getElementById("name").value;
+    var name_regex = "/
+]\
+vvvvvvvvvvvv"
 }
-
-console.log(shuma2);
